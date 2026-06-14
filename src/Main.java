25bec5a1b1bf684ad38f22a26d8b4110866f8cb0
Paul Rauen <paulrauen.portfolio@proton.me>
2026-06-15 00:30:12 +0200

    Main - creating the main application, calling a board

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame("Chess");
+        frame.setBackground(Color.gray);
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        frame.add(board);
+
+        frame.setVisible(true);
+
+
+    }
+}
