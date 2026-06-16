362df7bd07ab30f76283db5958b1dfc10288fcd4
Paul Rauen <paulrauen.portfolio@proton.me>
2026-06-16 14:36:56 +0200

    +Queen

---
+++ src/pieces/Queen.java
@@ -0,0 +1,20 @@
+package pieces;
+
+import main.Board;
+
+import java.awt.image.BufferedImage;
+
+public class Queen extends Piece{
+    public Queen(Board b, int col, int row, boolean isWhite) {
+        super(b);
+        this.col = col;
+        this.row = row;
+        this.xPos = col * b.tileSize;
+        this.yPos = row * b.tileSize;
+
+        this.isWhite = isWhite;
+        this.name = "Queen";
+
+        this.front = img.getSubimage(1 * imgScale, isWhite ? 0 : imgScale, imgScale, imgScale).getScaledInstance(b.tileSize, b.tileSize, BufferedImage.SCALE_SMOOTH);
+    }
+}
