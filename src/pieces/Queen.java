8f353e25a85c69ae05ba91bc9e7f597e7fc5a38f
Paul Rauen <paulrauen.portfolio@proton.me>
2026-06-17 00:35:17 +0200

    Queen - propper movement

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -17,4 +17,36 @@ public class Queen extends Piece{
 
         this.front = img.getSubimage(1 * imgScale, isWhite ? 0 : imgScale, imgScale, imgScale).getScaledInstance(b.getTileSize(), b.getTileSize(), BufferedImage.SCALE_SMOOTH);
     }
+
+    public boolean isValidMovement(int col, int row) {
+
+        int colDiff = Math.abs(col - this.col);
+        int rowDiff = Math.abs(row - this.row);
+
+        // horizontal OR vertical OR diagonal
+        return this.col == col
+                || this.row == row
+                || colDiff == rowDiff;
+    }
+
+    public boolean isValidCollide(int col, int row) {
+
+        int colStep = Integer.compare(col, this.col);
+        int rowStep = Integer.compare(row, this.row);
+
+        int c = this.col + colStep;
+        int r = this.row + rowStep;
+
+        while (c != col || r != row) {
+
+            if (b.getPiece(c, r) != null) {
+                return true;
+            }
+
+            c += colStep;
+            r += rowStep;
+        }
+
+        return false;
+    }
 }
