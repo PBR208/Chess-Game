ffe32974d49354f8e530cf3720611fe4c7c5fa26
Paul Rauen <paulrauen.portfolio@proton.me>
2026-06-20 02:48:53 +0200

    added a way to restart game after win, restartGame()

--- src/main/Board.java
+++ src/main/Board.java
@@ -31,39 +31,46 @@ public class Board extends JPanel{
         this.addMouseListener(input);
         this.addMouseMotionListener(input);
 
-        addPieces();
+        pieces = addPieces();
     }
 
-    public void addPieces(){
+    public ArrayList<Piece> addPieces(){
+
+        ArrayList<Piece> newGame = new ArrayList<>();
 
         // Black Pieces
 
-        pieces.add(new Rook(this, 0, 0, false));
-        pieces.add(new Rook(this, 7, 0, false));
-        pieces.add(new Knight(this, 1, 0, false));
-        pieces.add(new Knight(this, 6, 0, false));
-        pieces.add(new Bishop(this, 2, 0, false));
-        pieces.add(new Bishop(this, 5, 0, false));
-        pieces.add(new Queen(this, 3, 0, false));
-        pieces.add(new King(this, 4, 0, false));
+        newGame.add(new Rook(this, 0, 0, false));
+        newGame.add(new Rook(this, 7, 0, false));
+        newGame.add(new Knight(this, 1, 0, false));
+        newGame.add(new Knight(this, 6, 0, false));
+        newGame.add(new Bishop(this, 2, 0, false));
+        newGame.add(new Bishop(this, 5, 0, false));
+        newGame.add(new Queen(this, 3, 0, false));
+        newGame.add(new King(this, 4, 0, false));
 
         // White Pieces
 
-        pieces.add(new Rook(this, 0, 7, true));
-        pieces.add(new Rook(this, 7, 7, true));
-        pieces.add(new Knight(this, 1, 7, true));
-        pieces.add(new Knight(this, 6, 7, true));
-        pieces.add(new Bishop(this, 2, 7, true));
-        pieces.add(new Bishop(this, 5, 7, true));
-        pieces.add(new Queen(this, 3, 7, true));
-        pieces.add(new King(this, 4, 7, true));
+        newGame.add(new Rook(this, 0, 7, true));
+        newGame.add(new Rook(this, 7, 7, true));
+        newGame.add(new Knight(this, 1, 7, true));
+        newGame.add(new Knight(this, 6, 7, true));
+        newGame.add(new Bishop(this, 2, 7, true));
+        newGame.add(new Bishop(this, 5, 7, true));
+        newGame.add(new Queen(this, 3, 7, true));
+        newGame.add(new King(this, 4, 7, true));
 
         // Pawns
 
         for (int i = 0; i <= 7; i++) {
-            pieces.add(new Pawn(this, i, 1, false));
-            pieces.add(new Pawn(this, i, 6, true));
+            newGame.add(new Pawn(this, i, 1, false));
+            newGame.add(new Pawn(this, i, 6, true));
         }
+        return newGame;
+    }
+
+    public void restartGame(){
+        pieces = addPieces();
     }
 
     public void paintComponent(Graphics g){
@@ -212,13 +219,13 @@ public class Board extends JPanel{
             EndScreen screen = new EndScreen(parent, winner);
             screen.setVisible(true);
 
-            //restart game
+            restartGame();
 
         } else if(cs.isStalemate(nextPlayer)) {
             EndScreen screen = new EndScreen(parent, "Stalemate - Draw");
             screen.setVisible(true);
 
-            //restart game
+            restartGame();
         }
     }
 
