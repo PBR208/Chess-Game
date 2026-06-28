fafac6726248deeccc470b086ed30c8edea6f676
P.B.R. <paulrauen.portfolio@proton.me>
2026-06-28 23:26:08 +0200

    BUG: fixed calling super to ensure no visual artifact bugs

--- src/gui/Board.java
+++ src/gui/Board.java
@@ -73,6 +73,7 @@ public class Board extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
