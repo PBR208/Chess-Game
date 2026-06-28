21f74ddb11edac5746c52c5f46d96ec2ff453e22
P.B.R. <paulrauen.portfolio@proton.me>
2026-06-28 23:30:32 +0200

    BUG: fixed img being loaded 32 times

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -20,19 +20,21 @@ public class Piece {
 
     private boolean isFirstMove = true;
 
-    BufferedImage img = null;
+    protected static final BufferedImage img;
+    protected static final int imgScale;
 
-    {
+    static {
+        BufferedImage tmp = null;
         try {
-            img = ImageIO.read(Piece.class.getResourceAsStream("/pieces.png"));
-            if (img == null) throw new IOException("Resource not found");
+            tmp = ImageIO.read(Piece.class.getResourceAsStream("/pieces.png"));
+            if (tmp == null) throw new IOException("Resource not found");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Failed to load image from classpath.");
         }
+        img = tmp;
+        imgScale = (img != null) ? img.getWidth() / 6 : 0;
     }
-
-    protected final int imgScale = img.getWidth() / 6;
 
     public Image front;
     public Board b;
