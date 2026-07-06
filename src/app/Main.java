eb3463777e8a5d4e3e5c7a588fbcaabda3f5c71b
P.B.R. <paulrauen.portfolio@proton.me>
2026-07-06 17:17:10 +0200

    fixing public

--- src/app/Main.java
+++ src/app/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     private static JFrame frame;
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             frame = new JFrame("Chess");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
